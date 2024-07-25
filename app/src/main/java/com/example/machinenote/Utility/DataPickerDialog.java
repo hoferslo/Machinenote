a92708f7bb79d2983ba0eaea34b118ac80962097
hoferslo <93840083+hoferslo@users.noreply.github.com>
2024-07-25 14:48:34 +0200

    mhm

--- app/src/main/java/com/example/machinenote/Utility/DataPickerDialog.java
+++ app/src/main/java/com/example/machinenote/Utility/DataPickerDialog.java
@@ -2,11 +2,9 @@ package com.example.machinenote.Utility;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
+
 import com.example.machinenote.R;
 
 public class DataPickerDialog {
@@ -20,14 +18,10 @@ public class DataPickerDialog {
     public static int showDialog(View view, String title, String[] items, Context context, Button button, ListViewAdapter adapter, int whichListItem) {
         whichStringCache = -1;
 
-        // Inflate the default layout
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View dialogView = inflater.inflate(android.R.layout.select_dialog_item, null);
 
         // Set up the AlertDialog with custom style
         AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.CustomDialogTheme);
         builder.setTitle(title)
-                .setView(dialogView)
                 .setItems(items, (dialog, which) -> {
                     button.setText(items[which]);
                     whichStringCache = which;
