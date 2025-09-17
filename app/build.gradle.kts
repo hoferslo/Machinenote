e90a7ef7034b2afd05e54b078c70825b06f820ca
Tadej <tadej.kadivec1@gmail.com>
2025-09-17 13:51:18 +0200

    Povečana versionCode in versionName
    - versionCode spremenjen iz 9 na 10.
    - versionName spremenjen iz "1.2.3" na "1.2.4".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.machinenote"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.2.3"
+        versionCode = 10
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
