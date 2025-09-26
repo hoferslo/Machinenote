304cc3c3e6a28d739ebb156b61573b20d8e94f0c
Tadej <tadej.kadivec1@gmail.com>
2025-09-26 14:08:56 +0200

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.machinenote"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.2.4"
+        versionCode = 11
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
