a44cd50a8d7e802cb80f8fe20e95599a769ce405
Tadej <tadej.kadivec1@gmail.com>
2025-11-19 12:29:15 +0100

    Created a new fragment for kemikalije, also created a layout for this fragment which is complete - this is for the Department Razvoj

---
+++ app/src/main/java/com/example/machinenote/fragments/KemikalijeFragment.java
@@ -0,0 +1,11 @@
+package com.example.machinenote.fragments;
+
+import android.content.Context;
+
+import com.example.machinenote.BaseFragment;
+
+public class KemikalijeFragment extends BaseFragment {
+    private KemikalijeFragment binding;
+    private Context context;
+
+}
