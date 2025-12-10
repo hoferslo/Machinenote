44a06a9b4f2afb0cd6055bf5187f3915143f4bf8
Tadej <tadej.kadivec1@gmail.com>
2025-12-10 15:06:56 +0100

    Changed build to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.machinenote"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.2.9"
+        versionCode = 16
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
